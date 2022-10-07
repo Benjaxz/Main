670847d23dc796ec75a25d8d9c63068aa1f30416
Benjaxz <timyr.raximov@mail.ru>
2022-10-07 21:22:35 +0300

    comment

---
+++ Laba.py
@@ -0,0 +1,15 @@
+class Car():
+    def init(self, speed, power, health):
+        self.speed = speed
+        self.power = power
+        self.health = health
+
+    def about(self):
+        return self.speed
+
+
+
+Car1 = Car(1, 2, 3)
+Car2 = Car(10, 2, 3)
+
+print( Car1.about() > Car2.about() )
